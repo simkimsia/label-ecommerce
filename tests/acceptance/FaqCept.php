38cf5cd52fb7374274eceda636690748da65ee25
Aik Chun <aikchun616@gmail.com>
2013-06-26 00:12:28 +0800

    changed cept file

--- tests/acceptance/FaqCept.php
+++ tests/acceptance/FaqCept.php
@@ -2,4 +2,4 @@
 $I = new WebGuy($scenario);
 $I->wantTo('test contact form link is properly linked');
 $I->amOnPage('/faq');
-$I->seeLink('contact form','/call');
+$I->seeLink('contact form','call');
