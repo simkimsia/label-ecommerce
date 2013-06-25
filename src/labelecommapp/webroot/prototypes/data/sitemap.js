bd4870f913b64d0bd6b77b21e9b56ad784081af3
Aik Chun <aikchun616@gmail.com>
2013-06-25 19:18:55 +0800

    add edit products flow
    & fixed naming labels

--- src/labelecommapp/webroot/prototypes/data/sitemap.js
+++ src/labelecommapp/webroot/prototypes/data/sitemap.js
@@ -2,7 +2,7 @@
 
 (function() {
     var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
-    var _creator = function() { return _(b,[_(c,d,e,f,g,h),_(c,i,e,f,g,j)]);}; 
-var b="rootNodes",c="pageName",d="Use Cases",e="type",f="Flow",g="url",h="Use_Cases.html",i="Send email flow",j="Send_email_flow.html";
+    var _creator = function() { return _(b,[_(c,d,e,f,g,h),_(c,i,e,f,g,j),_(c,k,e,l,g,m)]);}; 
+var b="rootNodes",c="pageName",d="Use Cases",e="type",f="Flow",g="url",h="Use_Cases.html",i="Send email flow",j="Send_email_flow.html",k="Edit Product flow",l="Wireframe",m="Edit_Product_flow.html";
 return _creator();
 })();
