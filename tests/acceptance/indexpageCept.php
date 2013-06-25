df077587ee9abf662f3c173b8808e1e8edb9f8bc
kimsia <kimcity@gmail.com>
2013-06-25 08:37:30 +0800

    commit new acceptance tests for index page

---
+++ tests/acceptance/indexpageCept.php
@@ -0,0 +1,9 @@
+<?php
+$I = new WebGuy($scenario);
+$I->wantTo('ensure that the indexpage works');
+$I->amOnPage('/');
+//$I->seeElement('div.bannerHolder');
+//$I->seeElement('div.theme-default');
+//$I->seeElement('div.bannerHolder.theme-default');
+//$I->seeElement('div.featuredProducts');
+//$I->seeElement('.contentArea');
