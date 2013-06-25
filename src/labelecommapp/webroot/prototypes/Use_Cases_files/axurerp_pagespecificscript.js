94b375445ace034e0218e0b413cbf62c1481eb3a
kimsia <kimcity@gmail.com>
2013-06-25 18:16:03 +0800

    Add in the prototypes files

---
+++ src/labelecommapp/webroot/prototypes/Use_Cases_files/axurerp_pagespecificscript.js
@@ -0,0 +1,18 @@
+﻿for(var i = 0; i < 18; i++) { var scriptId = 'u' + i; window[scriptId] = document.getElementById(scriptId); }
+
+$axure.eventManager.pageLoad(
+function (e) {
+
+});
+gv_vAlignTable['u17'] = 'center';gv_vAlignTable['u15'] = 'center';gv_vAlignTable['u11'] = 'center';gv_vAlignTable['u13'] = 'center';gv_vAlignTable['u1'] = 'center';document.getElementById('u2_img').tabIndex = 0;
+
+u2.style.cursor = 'pointer';
+$axure.eventManager.click('u2', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('Send_email_flow.html');
+
+}
+});
+gv_vAlignTable['u3'] = 'center';gv_vAlignTable['u5'] = 'center';gv_vAlignTable['u7'] = 'center';gv_vAlignTable['u9'] = 'center';
---
+++ src/labelecommapp/webroot/prototypes/data/sitemap.js
@@ -0,0 +1,8 @@
+﻿var sitemap = 
+
+(function() {
+    var _ = function() { var r={},a=arguments; for(var i=0; i<a.length; i+=2) r[a[i]]=a[i+1]; return r; }
+    var _creator = function() { return _(b,[_(c,d,e,f,g,h),_(c,i,e,f,g,j),_(c,k,e,l,g,m,n,[_(c,o,e,l,g,p),_(c,q,e,l,g,r),_(c,s,e,l,g,t)])]);}; 
+var b="rootNodes",c="pageName",d="Use Cases",e="type",f="Flow",g="url",h="Use_Cases.html",i="Send email flow",j="Send_email_flow.html",k="Home",l="Wireframe",m="Home.html",n="children",o="Page 1",p="Page_1.html",q="Page 2",r="Page_2.html",s="Page 3",t="Page_3.html";
+return _creator();
+})();
