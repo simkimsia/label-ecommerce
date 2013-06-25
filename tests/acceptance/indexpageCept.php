b101a8bf5a1428e0301ded7ea29e46b822afa2a0
kimsia <kimcity@gmail.com>
2013-06-25 09:59:04 +0800

    updated the indexpageCept
    now it works on both development machines

--- tests/acceptance/indexpageCept.php
+++ tests/acceptance/indexpageCept.php
@@ -1,9 +1,8 @@
 <?php
+date_default_timezone_set('Asia/Singapore');
 $I = new WebGuy($scenario);
 $I->wantTo('ensure that the indexpage works');
 $I->amOnPage('/');
-//$I->seeElement('div.bannerHolder');
-//$I->seeElement('div.theme-default');
-//$I->seeElement('div.bannerHolder.theme-default');
-//$I->seeElement('div.featuredProducts');
-//$I->seeElement('.contentArea');
+$I->seeElement('div.bannerHolder.theme-default');
+$I->seeElement('div.featuredProducts');
+$I->seeElement('.contentArea');
