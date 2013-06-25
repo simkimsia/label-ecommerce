51441a272f148035cccc293d14d2982c215f21b0
kimsia <kimcity@gmail.com>
2013-06-26 00:49:47 +0800

    Redo the prototypes

--- src/labelecommapp/webroot/prototypes/Use_Cases_files/axurerp_pagespecificscript.js
+++ src/labelecommapp/webroot/prototypes/Use_Cases_files/axurerp_pagespecificscript.js
@@ -4,6 +4,17 @@ $axure.eventManager.pageLoad(
 function (e) {
 
 });
+document.getElementById('u20_img').tabIndex = 0;
+
+u20.style.cursor = 'pointer';
+$axure.eventManager.click('u20', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('Add_Product_flow.html');
+
+}
+});
 gv_vAlignTable['u21'] = 'center';document.getElementById('u22_img').tabIndex = 0;
 
 u22.style.cursor = 'pointer';
@@ -15,7 +26,18 @@ if (true) {
 
 }
 });
-gv_vAlignTable['u23'] = 'center';gv_vAlignTable['u25'] = 'center';gv_vAlignTable['u27'] = 'center';gv_vAlignTable['u29'] = 'center';gv_vAlignTable['u31'] = 'center';gv_vAlignTable['u1'] = 'center';document.getElementById('u2_img').tabIndex = 0;
+gv_vAlignTable['u23'] = 'center';document.getElementById('u24_img').tabIndex = 0;
+
+u24.style.cursor = 'pointer';
+$axure.eventManager.click('u24', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('Delete_Product_flow.html');
+
+}
+});
+gv_vAlignTable['u25'] = 'center';gv_vAlignTable['u27'] = 'center';gv_vAlignTable['u29'] = 'center';gv_vAlignTable['u31'] = 'center';gv_vAlignTable['u1'] = 'center';document.getElementById('u2_img').tabIndex = 0;
 
 u2.style.cursor = 'pointer';
 $axure.eventManager.click('u2', function(e) {
@@ -26,4 +48,15 @@ if (true) {
 
 }
 });
-gv_vAlignTable['u3'] = 'center';gv_vAlignTable['u5'] = 'center';gv_vAlignTable['u7'] = 'center';gv_vAlignTable['u9'] = 'center';gv_vAlignTable['u11'] = 'center';gv_vAlignTable['u13'] = 'center';gv_vAlignTable['u15'] = 'center';gv_vAlignTable['u17'] = 'center';gv_vAlignTable['u19'] = 'center';
+gv_vAlignTable['u3'] = 'center';gv_vAlignTable['u5'] = 'center';document.getElementById('u6_img').tabIndex = 0;
+
+u6.style.cursor = 'pointer';
+$axure.eventManager.click('u6', function(e) {
+
+if (true) {
+
+	self.location.href=$axure.globalVariableProvider.getLinkUrl('Customer_Login.html');
+
+}
+});
+gv_vAlignTable['u7'] = 'center';gv_vAlignTable['u9'] = 'center';gv_vAlignTable['u11'] = 'center';gv_vAlignTable['u13'] = 'center';gv_vAlignTable['u15'] = 'center';gv_vAlignTable['u17'] = 'center';gv_vAlignTable['u19'] = 'center';
