5388502b27c1fa11a02161a00eb4677a6d22dbd9
Aik Chun <aikchun616@gmail.com>
2013-06-27 22:43:30 +0800

    created product_group pages
    contains (admin_add, admin_edit, admin_index, admin_view, index, view)

---
+++ src/labelecommapp/Controller/ProductGroupsController.php
@@ -0,0 +1,107 @@
+<?php
+App::uses('AppController', 'Controller');
+/**
+ * ProductGroups Controller
+ *
+ * @property ProductGroup $ProductGroup
+ */
+class ProductGroupsController extends AppController {
+
+/***
+*
+*
+*
+*/
+	public function beforeFilter() {
+		parent::beforeFilter();
+		$this->Auth->allow();
+	}
+
+
+/**
+ * index method
+ *
+ * @return void
+ */
+	public function index() {
+		$this->ProductGroup->recursive = 0;
+		$this->set('productGroups', $this->paginate());
+	}
+
+/**
+ * view method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function view($id = null) {
+		if (!$this->ProductGroup->exists($id)) {
+			throw new NotFoundException(__('Invalid product group'));
+		}
+		$options = array('conditions' => array('ProductGroup.' . $this->ProductGroup->primaryKey => $id));
+		$this->set('productGroup', $this->ProductGroup->find('first', $options));
+	}
+
+/**
+ * add method
+ *
+ * @return void
+ */
+	public function add() {
+		if ($this->request->is('post')) {
+			$this->ProductGroup->create();
+			if ($this->ProductGroup->save($this->request->data)) {
+				$this->Session->setFlash(__('The product group has been saved'));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The product group could not be saved. Please, try again.'));
+			}
+		}
+	}
+
+/**
+ * edit method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function edit($id = null) {
+		if (!$this->ProductGroup->exists($id)) {
+			throw new NotFoundException(__('Invalid product group'));
+		}
+		if ($this->request->is('post') || $this->request->is('put')) {
+			if ($this->ProductGroup->save($this->request->data)) {
+				$this->Session->setFlash(__('The product group has been saved'));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The product group could not be saved. Please, try again.'));
+			}
+		} else {
+			$options = array('conditions' => array('ProductGroup.' . $this->ProductGroup->primaryKey => $id));
+			$this->request->data = $this->ProductGroup->find('first', $options);
+		}
+	}
+
+/**
+ * delete method
+ *
+ * @throws NotFoundException
+ * @param string $id
+ * @return void
+ */
+	public function delete($id = null) {
+		$this->ProductGroup->id = $id;
+		if (!$this->ProductGroup->exists()) {
+			throw new NotFoundException(__('Invalid product group'));
+		}
+		$this->request->onlyAllow('post', 'delete');
+		if ($this->ProductGroup->delete()) {
+			$this->Session->setFlash(__('Product group deleted'));
+			$this->redirect(array('action' => 'index'));
+		}
+		$this->Session->setFlash(__('Product group was not deleted'));
+		$this->redirect(array('action' => 'index'));
+	}
+}
---
+++ src/labelecommapp/Model/ProductGroup.php
@@ -0,0 +1,16 @@
+<?php
+App::uses('AppModel', 'Model');
+/**
+ * ProductGroup Model
+ *
+ */
+class ProductGroup extends AppModel {
+
+/**
+ * Display field
+ *
+ * @var string
+ */
+	public $displayField = 'name';
+
+}
