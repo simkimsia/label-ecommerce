14c5f0b93c524fff5cbfe143611e7533eea737a5
AikChun <aikchun616@gmail.com>
2013-09-28 11:31:15 +0800

    Removed $this->Auth->allow() and exported database ref #82

--- src/labelecommapp/Controller/ProductRepresentativeImagesController.php
+++ src/labelecommapp/Controller/ProductRepresentativeImagesController.php
@@ -7,10 +7,6 @@ App::uses('AppController', 'Controller');
  */
 class ProductRepresentativeImagesController extends AppController {
 
-	public function beforeFilter() {
-		parent::beforeFilter();
-		$this->Auth->allow();
-	}
 
 /**
  * index method
