ede8b08f0a6a78a91f4af948fcb161434f94e421
aikchun <aikchun616@gmail.com>
2014-12-03 17:23:59 +0800

    removed commented lines

--- src/labelecommapp/Lib/CartCalculator/CartCalculator.php
+++ src/labelecommapp/Lib/CartCalculator/CartCalculator.php
@@ -9,40 +9,6 @@ class CartCalculator {
 			$latestPrice = $productVariant->getLatestPriceOfProduct($item['foreign_key']);
 			$cart['CartsItem'][$key]['price'] = $latestPrice;
 		}
-		//$arrayOfIds = Hash::extract($cart['CartsItem'], '{n}.id');
-		// $options = [
-		// 	'conditions' => [
-		// 		'CartsItem.id' => $arrayOfIds,
-		// 	],
-		// 	'fields' => [
-		// 		'CartsItem.id', 'Product.price'
-		// 	],
-		// 	'joins' => [
-		// 		[
-		// 			'table' => 'product_variants',
-		// 			'alias' => 'ProductVariant',
-		// 			'type' => 'INNER',
-		// 			'conditions' => [
-		// 				'CartsItem.foreign_key = ProductVariant.id'
-		// 			]
-		// 		],
-		// 		[
-		// 			'table' => 'products',
-		// 			'alias' => 'Product',
-		// 			'type' => 'INNER',
-		// 			'conditions' => [
-		// 				'Product.id = ProductVariant.product_id'
-		// 			]
-		// 		],
-		// 	],
-		// ];
-		// $result = $cartModel->CartsItem->find('list', $options);
-
-		// foreach($cart['CartsItem'] as $key => $item) {
-		// 	$cartModel->log($item['id']);
-		// 	$cart['CartsItem'][$key]['price'] = $result[$item['id']];
-		// }
-
 
 		return $cart;
 	}
